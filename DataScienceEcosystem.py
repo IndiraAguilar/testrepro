5375d87aa8dbed1a2610670345b110f939d226f6
IndiraAguilar <emiliamicheo@gmail.com>
2023-09-04 01:24:17 -0600

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### *In this notebook, Data Science Tools and Ecosystem are summarized.*
+
+# **Objectives:**
+# 
+# - List of popular Languages for Data Sience
+# - List of some Data Science Tools
+# - Examples of arithmethic expressions
+
+# <details><summary> Some of the popular languages that Data Scientists use are:</summary>
+# 
+# 1. Python
+# 2. Java
+# 3. C++
+# 
+#     
+# </details>
+
+# # Data Science Tools
+# 
+# |Tools|
+# |-------|
+# |Jupyter Notebook|
+# |R Studio|
+# |GitHub|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[6]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+(200/60)
+
+
+# ## Author
+# Indira Emilia Aguilar Micheo
+
+# In[ ]:
+
+
+
+
